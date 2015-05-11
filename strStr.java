cf4503ff54e0d3b4580be46c337c429c6eb42167
nanwan03 <wn1842@gmail.com>
2015-05-10 21:52:15 -0400

    commit

---
+++ strStr.java
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * Returns a index to the first occurrence of target in source, or -1
+     * if target is not part of source.
+     * @param source string to be scanned.
+     * @param target string containing the sequence of characters to match.
+     */
+    public int strStr(String source, String target) {
+        //write your code here
+        if (source == null || target == null) {
+            return -1;
+        }
+        int j = 0;
+        for (int i = 0; i < source.length() - target.length() + 1; i++) {
+            for (j = 0; j < target.length(); j++) {
+                if (source.charAt(i + j) != target.charAt(j)) {
+                    break;
+                }
+            }
+            if (j == target.length()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
