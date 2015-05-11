bfbe174ff222886552c0aa58b34cb1fac81ba2d6
nanwan03 <wn1842@gmail.com>
2015-05-10 22:10:41 -0400

    commit

---
+++ Product of Array Exclude Itself.java
@@ -0,0 +1,24 @@
+public class Solution {
+    /**
+     * @param A: Given an integers array A
+     * @return: A Long array B and B[i]= A[0] * ... * A[i-1] * A[i+1] * ... * A[n-1]
+     */
+    public ArrayList<Long> productExcludeItself(ArrayList<Integer> A) {
+        ArrayList<Long> rst = new ArrayList<Long>();
+        if (A == null || A.size() < 2) {
+            return rst;
+        }
+        long product = 1;
+        for (int i : A) {
+            rst.add(product);
+            product *= i;
+        }
+        product = 1;
+        for (int i = A.size() - 1; i >= 0; i--) {
+            rst.set(i, product * rst.get(i));
+            product *= A.get(i);
+        }
+        return rst;
+    }
+}
+
