2f1f6e71fab1288a3f52f8ce8b6eb6a255f13344
nanwan03 <wn1842@gmail.com>
2015-05-10 22:14:33 -0400

    commit

---
+++ 3 Sum.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class Solution {
+    /**
+     * @param numbers : Give an array numbers of n integer
+     * @return : Find all unique triplets in the array which gives the sum of zero.
+     */
+    public ArrayList<ArrayList<Integer>> threeSum(int[] numbers) {
+        // write your code here
+        ArrayList<ArrayList<Integer>> rst = new ArrayList<ArrayList<Integer>>();
+        if (numbers == null || numbers.length == 0) {
+            return rst;
+        }
+        Arrays.sort(numbers);
+        for (int i = 0; i < numbers.length - 2; i++) {
+            if (i > 0 && numbers[i] == numbers[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = numbers.length - 1;
+            while (left < right) {
+                int sum = numbers[i] + numbers[left] + numbers[right];
+                if (sum == 0) {
+                    ArrayList<Integer> items = new ArrayList<Integer>();
+                    items.add(numbers[i]);
+                    items.add(numbers[left]);
+                    items.add(numbers[right]);
+                    rst.add(items);
+                    left++;
+                    right--;
+                    while (left < right && numbers[left] == numbers[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && numbers[right] == numbers[right + 1]) {
+                        right--;
+                    }
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return rst;
+    }
+}
