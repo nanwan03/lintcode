e30be753cd0f30dde7bc52e095fc0cc4012437fd
nanwan03 <wn1842@gmail.com>
2015-05-12 20:05:20 -0400

    commit

--- Find Peak Element II.java
+++ Find Peak Element II.java
@@ -10,63 +10,68 @@ class Solution {
     	}
     	return helper(A, 0, A.length - 1, 0, A[0].length - 1);
     }
-    private List<Integer> helper(int[][] A, int rStart, int rEnd, int cStart, int cEnd) {
-        List<Integer> rst = new ArrayList<Integer>(2);
-        int row = rEnd - rStart + 1;
-        int col = cEnd - cStart + 1;
+    private List<Integer> helper(int[][] A, int startR, int endR, int startC, int endC) {
+        int row = endR - startR + 1;
+        int col = endC - startC + 1;
         if (row == 1 && col == 1) {
-            rst.add(rStart);
-            rst.add(rEnd);
+            List<Integer> rst = new ArrayList<Integer>(2);
+            rst.add(startR);
+            rst.add(startC);
             return rst;
         }
-        int targetR = -1;
-        int targetC = -1;
-        int max = Integer.MIN_VALUE;
-        if (row > col) {
-            int midRow = rStart + (rEnd - rStart) / 2;
-            for (int i = Math.max(rStart, midRow - 1); i <= Math.min(rEnd, midRow + 1); i++) {
-                for (int j = cStart; j <= cEnd; j++) {
-                    if (A[i][j] > max) {
-                        max = A[i][j];
-                        targetR = i;
-                        targetC = j;
-                    }
-                }
-            }
-            if (targetR == midRow) {
-                rst.add(targetR);
-                rst.add(targetC);
-                return rst;
-            } else {
-                if (targetR < midRow) {
-                    return helper(A, rStart, midRow, cStart, cEnd);
-                } else {
-                    return helper(A, midRow, rEnd, cStart, cEnd);
-                }
-            }
+        int midC = startC + (endC - startC) / 2;
+        int midR = startR + (endR - startR) / 2;
+        
+        int targetR = findPeakRow(A, startR, endR, midC);
+        if (midC - 1 >= startC && A[targetR][midC - 1] > A[targetR][midC]) {
+            endC = midC - 1;
+        } else if (midC + 1 <= endC && A[targetR][midC + 1] > A[targetR][midC]) {
+            startC = midC + 1;
         } else {
-            int midCol = cStart + (cEnd - cStart) / 2;
-            for (int i = rStart; i <= rEnd; i++) {
-                for (int j = Math.max(cStart, midCol - 1); j <= Math.min(cEnd, midCol + 1); j++) {
-                    if (A[i][j] > max) {
-                        max = A[i][j];
-                        targetR = i;
-                        targetC = j;
-                    }
-                }
-            }
-            if (targetC == midCol) {
-                rst.add(targetR);
-                rst.add(targetC);
-                return rst;
-            } else {
-                if (targetC < midCol) {
-                    return helper(A, rStart, rEnd, cStart, midCol);
-                } else {
-                    return helper(A, rStart, rEnd, midCol, cEnd);
-                }
-            }
+            List<Integer> rst = new ArrayList<Integer>(2);
+            rst.add(targetR);
+            rst.add(midC);
+            return rst;
         }
+        
+        int targetC = findPeakCol(A, startC, endC, midR);
+        if (midR - 1 >= startR && A[midR - 1][targetC] > A[midR][targetC]) {
+            endR = midR - 1;
+        } else if (midR + 1 <= endR && A[midR + 1][targetC] > A[midR][targetC]) {
+            startR = midR + 1;
+        } else {
+            List<Integer> rst = new ArrayList<Integer>(2);
+            rst.add(midR);
+            rst.add(targetC);
+            return rst;
+        }
+        return helper(A, startR, endR, startC, endC);
+    }
+    public static int findPeakRow(int[][] A, int start, int end, int col) {
+    	while (start + 1 < end) {
+    		int mid = start + (end - start) / 2;
+    		if (A[mid - 1][col] < A[mid][col] && A[mid + 1][col] < A[mid][col]) {
+    			return mid;
+    		} else if (A[mid][col] < A[mid - 1][col]) {
+    			end = mid;
+    		} else {
+    			start = mid;
+    		}
+    	}
+    	return A[start][col] > A[end][col] ? start : end;
+    }
+    public static int findPeakCol(int[][] A, int start, int end, int row) {
+    	while (start + 1 < end) {
+    		int mid = start + (end - start) / 2;
+    		if (A[row][mid - 1] < A[row][mid] && A[row][mid + 1] < A[row][mid]) {
+    			return mid;
+    		} else if (A[row][mid] < A[row][mid - 1]) {
+    			end = mid; 
+    		} else {
+    			start = mid;
+    		}
+    	}
+    	return A[row][start] > A[row][end] ? start : end;
     }
 }
 
