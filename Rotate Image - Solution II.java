639f3fe928089d714cc3c78d43116fdf6b322417
nanwan03 <wn1842@gmail.com>
2015-05-17 10:45:00 -0400

    commit

---
+++ Rotate Image - Solution II.java
@@ -0,0 +1,27 @@
+public class Solution {
+    /**
+     * @param matrix: A list of lists of integers
+     * @return: Void
+     */
+    public void rotate(int[][] matrix) {
+        // write your code here
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+        int n = matrix.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n - i; ++j) {
+                int temp = matrix[n - 1 - j][n - 1 - i];
+                matrix[n - 1 - j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int temp = matrix[n - 1 - i][j];
+                matrix[n - 1 - i][j] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
