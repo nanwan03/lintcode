1a69f8a978a6c9ef3656b18c0f3c29a3b6b44276
unknown <nanwan03@NANWAN03-002251.usa.Arm.com>
2015-05-26 10:04:45 -0700

    commit

--- Update Bits.java
+++ Update Bits.java
@@ -6,31 +6,9 @@ class Solution {
      */
     public int updateBits(int n, int m, int i, int j) {
         // write your code here
-       if (i == 0 && j == 31) {
-    	   return m;
-       }
-       if (m == 0) {
-    	   for (int k = i; k <= j; k++) {
-    		   n = n & ~(1 << k);
-    	   }
-    	   return n;
-       }
-       if (i == 0 && j == 31) {
-           return m;
-       }
-        int max = ~0; /* All 1¡¯s */ 
-
-        // 1¡¯s through position j, then 0¡¯s 
-        int left = max - ((1 << (j+1)) - 1); // here should be j+1 not j. 
-        
-        // 1¡¯s after position i 
-        int right = ((1 << i) - 1); 
-        
-        // 1¡¯s, with 0s between i and j 
-        int mask = left | right; 
-        
-        // Clear i through j, then put m in there 
-        return (n & mask) | (m << i); 
+       int right_part = n & ((1 << i) - 1);
+        // The behavior of right shift >= 32 is undefined in C++.
+        int left_part = j >= 31 ? 0 : (n >> (j + 1)) << (j + 1);
+        return left_part | (m << i) | right_part;
     }
 }
-
