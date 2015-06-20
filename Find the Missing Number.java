6d183a4c5237742341fd2f3377469b411697fec6
nanwan03 <wn1842@gmail.com>
2015-06-20 16:08:17 -0400

    commit

---
+++ Find the Missing Number.java
@@ -0,0 +1,28 @@
+public class Solution {
+    /**    
+     * @param nums: an array of integers
+     * @return: an integer
+     */
+    public int findMissing(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return 1;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length - 1 && nums[i] != i + 1) {
+                int temp = nums[nums[i] - 1];
+                if (temp == nums[i]) {
+                    break;
+                }
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return 0;
+    }
+}
