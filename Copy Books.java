fa0535533a0a4e783580fe1814e313cf5f18f310
nanwan03 <wn1842@gmail.com>
2015-07-18 11:43:20 -0400

    commit

---
+++ Copy Books.java
@@ -0,0 +1,50 @@
+public class Solution {
+    /**
+     * @param pages: an array of integers
+     * @param k: an integer
+     * @return: an integer
+     */
+    public int copyBooks(int[] pages, int k) {
+        // write your code here
+        int max = 0;
+        int sum = 0;
+        for (int page : pages) {
+            max = Math.max(max, page);
+            sum += page;
+        }
+        if (k >= pages.length) {
+            return max;
+        }
+        int average = sum / k;
+        return binarySearch(pages, k, average, sum);
+    }
+    private static int binarySearch(int[] pages, int k, int left, int right) {
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (valid(pages, k, mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        if (valid(pages, k, left)) {
+        	return left;
+        } else if (valid(pages, k, right)) {
+        	return right;
+        } else {
+        	return right + 1;
+        }
+    }
+    private static boolean valid(int[] pages, int k, int page) {
+        int sum = 0;
+        int people = 0;
+        for (int i = 0; i < pages.length && people < k; ++i) {
+            if (sum + pages[i] > page) {
+                sum = 0;
+                ++people;
+            }
+            sum += pages[i];
+        }
+        return people < k && sum <= page;
+    }
+}
