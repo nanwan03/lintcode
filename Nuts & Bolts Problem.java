a26a05dc3a418c5c75bf97deb3e3ebc1691e79c4
nanwan03 <wn1842@gmail.com>
2015-07-24 09:34:51 -0400

    commit

--- Nuts & Bolts Problem.java
+++ Nuts & Bolts Problem.java
@@ -1,8 +1,8 @@
 /**
- * public class Compare {
- *     public static int cmp(int a, int b);
+ * public class NBCompare {
+ *     public int cmp(String a, String b);
  * }
- * You can use Compare.cmp(a, b) to compare nuts "a" and bolts "b",
+ * You can use compare.cmp(a, b) to compare nuts "a" and bolts "b",
  * if "a" is bigger than "b", it will return 1, else if they are equal,
  * it will return 0, else if "a" is smaller than "b", it will return -1.
  * When "a" is not a nut or "b" is not a bolt, it will return 2, which is not valid.
@@ -11,56 +11,45 @@ public class Solution {
     /**
      * @param nuts: an array of integers
      * @param bolts: an array of integers
+     * @param compare: a instance of Comparator
      * @return: nothing
      */
-    public void sortNutsAndBolts(int[] nuts, int[] bolts) {
+    public void sortNutsAndBolts(String[] nuts, String[] bolts, NBComparator compare) {
         // write your code here
-        quickSort(nuts, bolts, 0, nuts.length - 1);
+        quickSort(nuts, bolts, 0, nuts.length - 1, compare);
     }
-    private void quickSort(int[] nuts, int[] bolts, int left, int right) {
-        if (left >= right) {
-          return;
+    private void quickSort(String[] nuts, String[] bolts, int left, int right, NBComparator  compare) {
+        if (left < right) {
+            //Random r = new Random();
+            //int pivot = r.nextInt(right - left + 1) + left;
+            int pivot = partition(nuts, left, right, bolts[right], compare);
+            partition(bolts, left, right, nuts[pivot], compare);
+            quickSort(nuts, bolts, left, pivot - 1, compare);
+            quickSort(nuts, bolts, pivot + 1, right, compare);
         }
-        Random r = new Random();
-        int pivot = r.nextInt(right - left + 1) + left;
-        pivot = partition2(nuts, left, right, bolts[pivot]);
-        partition2(bolts, left, right, nuts[pivot]);
-        quickSort(nuts, bolts, left, pivot - 1);
-        quickSort(nuts, bolts, pivot + 1, right);
   }
-  private int partition(int[] array, int left, int right, int target) {
-      for (int i = left; i < right;) {
-          if (Compare.cmp(array[i], target) == -1 ||
-                (Compare.cmp(array[i], target) == 2 && Compare.cmp(target, array[i]) == 1)) {
-                swap(array, left++, i++);  
-            } else if (Compare.cmp(array[i], target) == 1 ||
-                (Compare.cmp(array[i], target) == 2 && Compare.cmp(target, array[i]) == -1)) {
+  private int partition(String[] arr, int left, int right, String pivot, NBComparator compare) {
+        for (int i = left; i < right; ) {
+            if (compare.cmp(arr[i], pivot) == -1 ||  // Smaller.
+                (compare.cmp(arr[i], pivot) == 2 &&
+                 compare.cmp(pivot, arr[i]) == 1)) {
+                swap(arr, left++, i++);
+            } else if (compare.cmp(arr[i], pivot) == 1 ||  // Bigger.
+                      (compare.cmp(arr[i], pivot) == 2 &&
+                       compare.cmp(pivot, arr[i]) == -1)) {
                 ++i;
-            } else {
-                swap(array, i, right);
+            } else {  // Equal.
+                swap(arr, i, right);
             }
-      }
-      swap(array, left, right);
-      return left;
-        
+        }
+        // Put the pivot to the partition index.
+        swap(arr, left, right);
+
+        // Return the partition index of an array.
+        return left;
   }
-  private int partition2(int[] array, int left, int right, int target) {
-      int cur = left;
-      while (cur <= right) {
-          if (Compare.cmp(array[cur], target) == -1 ||
-                (Compare.cmp(array[cur], target) == 2 && Compare.cmp(target, array[cur]) == 1)) {
-                    swap(array, left++, cur++);
-            } else if (Compare.cmp(array[cur], target) == 1 ||
-                (Compare.cmp(array[cur], target) == 2 && Compare.cmp(target, array[cur]) == -1)) {
-                    swap(array, cur, right--);
-            } else {
-                cur++;
-            } 
-      }
-      return left;
-  }
-  private void swap(int[] array, int i, int j) {
-        int temp = array[i];
+  private void swap(String[] array, int i, int j) {
+        String temp = array[i];
         array[i] = array[j];
         array[j] = temp;
   }
