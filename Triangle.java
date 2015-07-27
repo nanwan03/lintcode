6e11ddc33843ef78898e086fb48af820c78f53eb
nanwan03 <wn1842@gmail.com>
2015-07-27 16:44:49 -0400

    commit

--- Triangle.java
+++ Triangle.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class Solution {
     /**
      * @param triangle: a list of lists of integers.
@@ -7,20 +5,20 @@ public class Solution {
      */
     public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
         // write your code here
-        if (triangle == null || triangle.size() == 0) {
+    	if (triangle == null || triangle.size() == 0) {
             return 0;
         }
         int size = triangle.size();
-        int[][] dp = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            dp[size - 1][i] = triangle.get(size - 1).get(i);
+        int[] dp = new int[size];
+        for (int i = 0; i < size; ++i) {
+            dp[i] = triangle.get(size - 1).get(i);
         }
-        for (int i = size - 2; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle.get(i).get(j);
+        for (int i = size - 2; i >= 0; --i) {
+            for (int j = 0; j <= i; ++j) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
 }
 
