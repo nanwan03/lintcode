1ac3067c8c27c21e7a9662810f1fad9df6dd54bb
nanwan03 <wn1842@gmail.com>
2015-10-10 23:12:22 -0400

    commit

---
+++ Implement Trie.java
@@ -0,0 +1,63 @@
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie trie = new Trie();
+ * trie.insert("lintcode");
+ * trie.search("lint"); will return false
+ * trie.startsWith("lint"); will return true
+ */
+class TrieNode {
+    TrieNode[] next;
+    boolean end;
+    // Initialize your data structure here.
+    public TrieNode() {
+        next = new TrieNode[26];
+        end = false;
+    }
+}
+
+public class Solution {
+    private TrieNode root;
+
+    public Solution() {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.next[c - 'a'] == null) {
+                node.next[c - 'a'] = new TrieNode();
+            }
+            node = node.next[c - 'a'];
+        }
+        node.end = true;
+    }
+
+    // Returns if the word is in the trie.
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.next[c - 'a'] != null) {
+                node = node.next[c - 'a'];
+            } else {
+                return false;
+            }
+        }
+        return node.end;
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if (node.next[c - 'a'] != null) {
+                node = node.next[c - 'a'];
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
