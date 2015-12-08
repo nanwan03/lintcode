2fef5e3ebfc38fb52acbb026aa730d48dbdbba21
nanwan03 <wn1842@gmail.com>
2015-12-07 22:42:09 -0500

    commit

---
+++ Tweaked Identical Binary Tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param a, b, the root of binary trees.
+     * @return true if they are tweaked identical, or false.
+     */
+    public boolean isTweakedIdentical(TreeNode a, TreeNode b) {
+        // Write your code here
+        if (a == null && b == null) {
+          return true;
+        }
+        if (a == null || b == null) {
+          return false;
+        }
+        if (a.val != b.val) {
+          return false;
+        }
+        return (isTweakedIdentical(a.left, b.right) && isTweakedIdentical(a.right, b.left)) || (isTweakedIdentical(a.left, b.left) && isTweakedIdentical(a.right, b.right));
+        }
+}
