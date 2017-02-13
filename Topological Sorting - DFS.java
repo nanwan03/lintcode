2c3f77cb046d1d8c0c407c1c0197847a61dc169c
nanwan03 <wn1842@gmail.com>
2017-02-13 15:11:32 -0500

    commit

--- Topological Sorting - DFS.java
+++ Topological Sorting - DFS.java
@@ -10,41 +10,37 @@ public class Solution {
     /**
      * @param graph: A list of Directed graph node
      * @return: Any topological order for the given graph.
-     */
-	private int timeF = 1;
+     */   
     public ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
         // write your code here
-    	ArrayList<DirectedGraphNode> rst = new ArrayList<DirectedGraphNode>();
-    	if (graph == null || graph.size() == 0) {
-    		return rst;
-    	}
-    	Map<DirectedGraphNode, Integer> map = new HashMap<DirectedGraphNode, Integer>();
-    	for (DirectedGraphNode node : graph) {
-    		map.put(node, 0);
-    		rst.add(node);
-    	}
-    	int[] time = new int[graph.size()];
-    	DFS(map, graph, time);
-    	for (DirectedGraphNode node : graph) {
-    		rst.set(graph.size() - time[node.label], node);
-    	}
-    	return rst;
+        ArrayList<DirectedGraphNode> rst = new ArrayList<DirectedGraphNode>();
+        if (graph == null || graph.size() == 0) {
+            return rst;
+        }
+        Map<DirectedGraphNode, Integer> visited = new HashMap<DirectedGraphNode, Integer>();
+        for (DirectedGraphNode node : graph) {
+            visited.put(node, 0);
+        }
+        Stack<DirectedGraphNode> stack = new Stack<DirectedGraphNode>();
+        for (DirectedGraphNode node : graph) {
+            DFS(node, visited, graph, stack);
+        }
+        while (!stack.isEmpty()) {
+            rst.add(stack.pop());
+        }
+        return rst;
     }
-    private void DFS(Map<DirectedGraphNode, Integer> map, ArrayList<DirectedGraphNode> graph, int[] time) {
-    	for (DirectedGraphNode node : graph) {
-    		if (map.get(node) == 0) {
-    			DFS(node, map, graph, time);
-    		}
-    	}
-    }
-    private void DFS(DirectedGraphNode node, Map<DirectedGraphNode, Integer> map, ArrayList<DirectedGraphNode> graph, int[] time) {
-    	map.put(node, 1);
-    	for (DirectedGraphNode neighbor : node.neighbors) {
-    		if (map.get(neighbor) == 0) {
-    			DFS(neighbor, map, graph, time);
-    		}
-    	}
-    	map.put(node, 2);
-    	time[node.label] = timeF++;
+    private void DFS(DirectedGraphNode node, Map<DirectedGraphNode, Integer> map, List<DirectedGraphNode> graph, Stack<DirectedGraphNode> stack) {
+        if (map.get(node) != 0) {
+            return;
+        }
+        map.put(node, 1);
+        for (DirectedGraphNode neighbor : node.neighbors) {
+            if (map.get(neighbor) == 0) {
+                DFS(neighbor, map, graph, stack);
+            }
+        }
+        map.put(node, 2);
+        stack.push(node);
     }
 }
