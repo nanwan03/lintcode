832ff151bf02a91834385ee3342e6181ee3b8293
nanwan03 <wn1842@gmail.com>
2017-03-06 22:57:07 -0500

    commit

--- Heapify.java
+++ Heapify.java
@@ -13,24 +13,21 @@ public class Solution {
         }
     }
     private void adjust(int[] A, int i) {
-    int size = A.length;
-    	//while (i < size / 2) {
-    		int min = i;
-    		int left = i * 2 + 1;
-    		int right = i * 2 + 2;
-    		if (left < size && A[left] < A[min]) {
-    			min = left;
-    		}
-    		if (right < size && A[right] < A[min]) {
-    			min = right;
-    		}
-    		if (i == min) {
-    			return;
-    		}
-    		swap(A, min, i);
-    		adjust(A, min);
-    	//	i = min;
-    //	}
+    	int size = A.length;
+		int min = i;
+		int left = i * 2 + 1;
+		int right = i * 2 + 2;
+		if (left < size && A[left] < A[min]) {
+			min = left;
+		}
+		if (right < size && A[right] < A[min]) {
+			min = right;
+		}
+		if (i == min) {
+			return;
+		}
+		swap(A, min, i);
+		adjust(A, min);
     }
     private void swap(int[] A, int i, int j) {
     	int temp = A[i];
