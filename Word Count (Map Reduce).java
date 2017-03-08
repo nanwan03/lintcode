be169e18cedc5c5198ed8eb889daead37b423bef
nanwan03 <wn1842@gmail.com>
2017-03-07 22:00:34 -0500

    commit

---
+++ Word Count (Map Reduce).java
@@ -0,0 +1,36 @@
+/**
+ * Definition of OutputCollector:
+ * class OutputCollector<K, V> {
+ *     public void collect(K key, V value);
+ *         // Adds a key/value pair to the output buffer
+ * }
+ */
+public class WordCount {
+
+    public static class Map {
+        public void map(String key, String value, OutputCollector<String, Integer> output) {
+            // Write your code here
+            // Output the results into output buffer.
+            // Ps. output.collect(String key, int value);
+            String[] tokens = value.split(" ");
+
+            for(String word : tokens) {
+                output.collect(word, 1);
+            }
+        }
+    }
+
+    public static class Reduce {
+        public void reduce(String key, Iterator<Integer> values,
+                           OutputCollector<String, Integer> output) {
+            // Write your code here
+            // Output the results into output buffer.
+            // Ps. output.collect(String key, int value);
+            int sum = 0;
+            while (values.hasNext()) {
+                sum += values.next();
+            }
+            output.collect(key, sum);
+        }
+    }
+}
