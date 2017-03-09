11e4fa53060d581f7df3cb21ab8e5d32103679d0
nanwan03 <wn1842@gmail.com>
2017-03-09 16:40:35 -0500

    commit

---
+++ Maximum Average Subarray.java
@@ -0,0 +1,42 @@
+public class Solution {
+    /**
+     * @param nums an array with positive and negative numbers
+     * @param k an integer
+     * @return the maximum average
+     */
+    public double maxAverage(int[] nums, int k) {
+        // Write your code here
+        double l = Integer.MAX_VALUE, r = Integer.MIN_VALUE;
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] < l)
+                l = nums[i];
+            if (nums[i] > r)
+                r = nums[i];
+        }
+
+        double[] sum = new double[n + 1];
+        sum[0] = 0;
+        while (r - l >= 1e-6) {
+            double mid = (l + r) / 2.0;
+
+            double min_pre = 0;
+            boolean check = false;
+            for (int i = 1; i <= n; ++i) {
+                sum[i] = sum[i - 1] + nums[i - 1] - mid;
+                if (i >= k && sum[i] - min_pre >= 0) {
+                    check = true;
+                    break;
+                }
+                if (i >= k)
+                    min_pre = Math.min(min_pre, sum[i - k + 1]);
+            }
+            if (check)
+                l = mid;
+            else
+                r = mid;
+        }
+
+        return l;
+    }
+}
