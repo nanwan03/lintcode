61553cc342382fcabf78910189c70bf8137cb7bb
nanwan03 <wn1842@gmail.com>
2017-03-10 14:40:33 -0500

    commit

--- Maximum Subarray III.java
+++ Maximum Subarray III.java
@@ -4,28 +4,28 @@ public class Solution {
      * @param k: An integer denote to find k non-overlapping subarrays
      * @return: An integer denote the sum of max k non-overlapping subarrays
      */
-    public int maxSubArray(ArrayList<Integer> nums, int k) {
-        if (nums == null || nums.size() == 0) {
+    public int maxSubArray(int[] nums, int k) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
         	return 0;
         }
-        int[][] dp = new int[nums.size() + 1][k + 1];
-        for (int i = 0; i <= nums.size(); i++) {
+        int[][] dp = new int[nums.length + 1][k + 1];
+        for (int i = 0; i <= nums.length; i++) {
         	dp[i][0] = 0;
         	for (int j = 1; j <= k; j++) {
         		dp[i][j] = Integer.MIN_VALUE;
         	}
         }
-        for (int i = 1; i <= nums.size(); i++) {
+        for (int i = 1; i <= nums.length; i++) {
         	for (int j = 1; j <= Math.min(i, k); j++) {
         		int temp = 0;
         		dp[i][j] = dp[i - 1][j];
         		for (int p = i; p >= j; p--) {
-        			temp = Math.max(temp, 0) + nums.get(p - 1);
+        			temp = Math.max(temp, 0) + nums[p - 1];
         			dp[i][j] = Math.max(dp[i][j], dp[p - 1][j - 1] + temp);
         		}
         	}
         }
-        return dp[nums.size()][k];
+        return dp[nums.length][k];
     }
 }
-
