01a0d6c1931d2cd3e50f03d91ed6a045fb09c2e8
nanwan03 <wn1842@gmail.com>
2017-03-12 14:52:46 -0400

    commit

--- Coins in a Line II.java
+++ Coins in a Line II.java
@@ -2,24 +2,28 @@ public class Solution {
     /**
      * @param values: an array of integers
      * @return: a boolean which equals to true if the first player will win
-     * Memory Limit Exceed
      */
     public boolean firstWillWin(int[] values) {
         // write your code here
-        if (values == null || values.length == 0) {
-            return false;
+        // dp 表示从i到end 的最大值
+        // int values[] ={1,2,4,3,4,8,5,6,12};
+        int len = values.length;
+        // 长度小于2的时候第一个人一定获胜
+        if(len <= 2)
+            return true;
+        int dp[] = new int[len+1];
+        dp[len] = 0;
+        dp[len-1] = values[len-1];
+        dp[len-2] = values[len-1] + values[len - 2];
+        dp[len - 3] = values[len-3] + values[len - 2];
+        for(int i = len -4;i>=0;i--){
+            dp[i] = values[i] + Math.min(dp[i+2],dp[i+3]);
+            dp[i] = Math.max(dp[i],values[i]+values[i+1]+ Math.min(dp[i+3],dp[i+4]));
+
         }
-        int[] dp = new int[5];
-        Arrays.fill(dp, 0);
         int sum = 0;
-        for (int i = values.length - 1; i >= 0; i--) {
-            sum += values[i];
-            int a = i + 2 < values.length ? dp[(i + 2) % 5] : 0;
-            int b = i + 3 < values.length ? dp[(i + 3) % 5] : 0;
-            int c = i + 4 < values.length ? dp[(i + 4) % 5] : 0;
-            dp[i % 5] = Math.max(values[i] + Math.min(a, b), 
-                            values[i] + ((i + 1) < values.length ? values[i + 1] : 0) + Math.min(b, c));
-        }
+        for(int a:values)
+            sum +=a;
         return dp[0] > sum - dp[0];
     }
 }
