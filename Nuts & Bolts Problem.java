fa5399908f9e158f8ed85b72a5358e45d87eed09
nanwan03 <wn1842@gmail.com>
2017-03-14 23:28:58 -0400

    commit

--- Nuts & Bolts Problem.java
+++ Nuts & Bolts Problem.java
@@ -22,29 +22,28 @@ public class Solution {
         if (left >= right) {
             return;
         }
-        int partitionIndex = partition(nuts, left, right, bolts[right], compare);
-        partition(bolts, left, right, nuts[partitionIndex], compare);
-        quickSort(nuts, bolts, left, partitionIndex - 1, compare);
-        quickSort(nuts, bolts, partitionIndex + 1, right, compare);
+        Random r = new Random();
+        int p = partition(nuts, left, right, bolts[r.nextInt(right - left) + left], compare);
+        partition(bolts, left, right, nuts[p], compare);
+        quickSort(nuts, bolts, left, p - 1, compare);
+        quickSort(nuts, bolts, p + 1, right, compare);
     }
-    private void swap(String[] array, int i, int j) {
-        String tmp = array[i];
-        array[i] = array[j];
-        array[j] = tmp;
-    }
-    private int partition(String[] strings, int left, int right, String partitionValue, NBComparator compare) {
+    private int partition(String[] array, int left, int right, String pivotValue, NBComparator compare) {
         int cur = left;
         while (cur <= right) {
-            if (compare.cmp(strings[cur], partitionValue) == -1 ||
-                (compare.cmp(strings[cur], partitionValue) == 2 && compare.cmp(partitionValue, strings[cur]) == 1)) {
-                    swap(strings, cur++, left++);
-                } else if (compare.cmp(strings[cur], partitionValue) == 1 ||
-                            (compare.cmp(strings[cur], partitionValue) == 2 && compare.cmp(partitionValue, strings[cur]) == -1)) {
-                                swap(strings, cur, right--);
-                    } else {
-                        cur++;
-                    }
+            if (compare.cmp(array[cur], pivotValue) == -1 || compare.cmp(pivotValue, array[cur]) == 1) {
+                swap(array, cur++, left++);
+            } else if (compare.cmp(array[cur], pivotValue) == 1 || compare.cmp(pivotValue, array[cur]) == -1) {
+                swap(array, cur, right--);
+            } else {
+                cur++;
+            }
         }
         return left;
+    }
+    private void swap(String[] str, int i, int j) {
+        String tmp = str[i];
+        str[i] = str[j];
+        str[j] = tmp;
     }
 };
