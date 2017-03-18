0138c68eb795f5fbb722c48d64dd233154f02f46
nanwan03 <wn1842@gmail.com>
2017-03-18 17:28:24 -0400

    commit

--- Copy Books.java
+++ Copy Books.java
@@ -6,43 +6,42 @@ public class Solution {
      */
     public int copyBooks(int[] pages, int k) {
         // write your code here
-        int max = 0;
         int sum = 0;
-        for (int page : pages) {
-            max = Math.max(max, page);
-            sum += page;
+        int max = 0;
+        for (int i : pages) {
+            sum += i;
+            max = Math.max(max, i);
         }
         if (k >= pages.length) {
             return max;
         }
         int average = sum / k;
-        return binarySearch(pages, k, average, sum);
+        return helper(pages, k, average, sum);
     }
-    private static int binarySearch(int[] pages, int k, int left, int right) {
+    private int helper(int[] pages, int k, int left, int right) {
         while (left + 1 < right) {
             int mid = left + (right - left) / 2;
-            if (valid(pages, k, mid)) {
+            if (isValid(pages, k, mid)) {
                 right = mid;
             } else {
                 left = mid;
             }
         }
-        if (valid(pages, k, left)) {
-        	return left;
-        } else {
-        	return right;
-        } 
+        if (isValid(pages, k, left)) {
+            return left;
+        }
+        return right;
     }
-    private static boolean valid(int[] pages, int k, int page) {
+    private boolean isValid(int[] pages, int k, int page) {
         int sum = 0;
-        int people = 0;
-        for (int i = 0; i < pages.length && people < k; ++i) {
+        int people = 1;
+        for (int i = 0; i < pages.length && people <= k; ++i) {
             if (sum + pages[i] > page) {
                 sum = 0;
-                ++people;
+                people++;
             }
             sum += pages[i];
         }
-        return people < k && sum <= page;
+        return sum <= page && people <= k;
     }
 }
