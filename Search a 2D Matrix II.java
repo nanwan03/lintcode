3748d65c1635df65d36309f383e17ebe541ad335
nanwan03 <wn1842@gmail.com>
2017-03-18 16:53:31 -0400

    commit

--- Search a 2D Matrix II.java
+++ Search a 2D Matrix II.java
@@ -4,18 +4,18 @@ public class Solution {
      * @param: A number you want to search in the matrix
      * @return: An integer indicate the occurrence of target in the given matrix
      */
-    public int searchMatrix(ArrayList<ArrayList<Integer>> matrix, int target) {
-        // write your code
-        if (matrix == null || matrix.size() == 0) {
+    public int searchMatrix(int[][] matrix, int target) {
+        // write your code here
+        if (matrix == null || matrix.length == 0) {
             return 0;
         }
-        int row = matrix.size();
-        int col = matrix.get(0).size();
+        int row = matrix.length;
+        int col = matrix[0].length;
         int rIndex = 0;
         int cIndex = col - 1;
         int count = 0;
         while (rIndex < row && cIndex >= 0) {
-            int temp = matrix.get(rIndex).get(cIndex);
+            int temp = matrix[rIndex][cIndex];
             if (temp == target) {
                 count++;
                 rIndex++;
@@ -29,4 +29,3 @@ public class Solution {
         return count;
     }
 }
-
--- Search a 2D Matrix.java
+++ Search a 2D Matrix.java
@@ -1,23 +1,21 @@
-import java.util.ArrayList;
-
 public class Solution {
     /**
      * @param matrix, a list of lists of integers
      * @param target, an integer
      * @return a boolean, indicate whether matrix contains target
      */
-    public boolean searchMatrix(ArrayList<ArrayList<Integer>> matrix, int target) {
-        // write your code
-        if (matrix == null || matrix.size() == 0) {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // write your code here
+        if (matrix == null || matrix.length == 0) {
             return false;
         }
-        int rows = matrix.size();
-        int cols = matrix.get(0).size();
+        int rows = matrix.length;
+        int cols = matrix[0].length;
         int start = 0;
         int end = rows * cols - 1;
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
-            int digit = matrix.get(mid / cols).get(mid % cols);
+            int digit = matrix[mid / cols][mid % cols];
             if (digit == target) {
                 return true;
             } else if (digit < target) {
@@ -26,13 +24,12 @@ public class Solution {
                 end = mid;
             }
         }
-        if (matrix.get(start / cols).get(start % cols) == target) {
+        if (matrix[start / cols][start % cols] == target) {
             return true;
         }
-        if (matrix.get(end / cols).get(end % cols) == target) {
+        if (matrix[end / cols][end % cols] == target) {
             return true;
         }
         return false;
     }
 }
-
