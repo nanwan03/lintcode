e02d2e207e324ccdf5f8c15fd9732532b9d6605c
nanwan03 <wn1842@gmail.com>
2017-03-18 16:14:28 -0400

    commit

--- Word Ladder.java
+++ Word Ladder.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class Solution {
     /**
       * @param start, a string
@@ -12,36 +10,33 @@ public class Solution {
         if (dict == null || dict.size() == 0) {
             return 0;
         }
-        if (start.equals(end)) {
-            return 1;
-        }
+        int rst = 1;
         Queue<String> queue = new LinkedList<String>();
         queue.offer(start);
         dict.remove(start);
-        int step = 1;
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()) {
             int size = queue.size();
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < size; ++i) {
                 String str = queue.poll();
-                for (int j = 0; j < str.length(); j++) {
-                    for (char c = 'a'; c <= 'z'; c++) {
+                if (str.equals(end)) {
+                    return rst;
+                }
+                for (int j = 0; j < str.length(); ++j) {
+                    for (char c = 'a'; c <= 'z'; ++c) {
                         if (c == str.charAt(j)) {
                             continue;
                         }
-                        String temp = replace(str, j, c);
-                        if (temp.equals(end)) {
-                            return step + 1;
-                        }
-                        if (dict.contains(temp)) {
-                            queue.offer(temp);
-                            dict.remove(temp);
+                        String newStr = replace(str, j, c);
+                        if (dict.contains(newStr) || newStr.equals(end)) {
+                            queue.offer(newStr);
+                            dict.remove(newStr);
                         }
                     }
                 }
             }
-            step++;
+            rst++;
         }
-        return 0;
+        return rst;
     }
     private String replace(String str, int index, char c) {
         char[] chars = str.toCharArray();
