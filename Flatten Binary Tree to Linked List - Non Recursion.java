b40d4f40ad19ab777f5f325d1918d9a88939b2fc
nanwan03 <wn1842@gmail.com>
2017-03-19 18:28:31 -0400

    commit

---
+++ Flatten Binary Tree to Linked List - Non Recursion.java
@@ -0,0 +1,40 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    public void flatten(TreeNode root) {
+        // write your code here
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            node.left = null;
+            if (stack.isEmpty()) {
+                node.right = null;
+            } else {
+                node.right = stack.peek();
+            }
+        }
+    }
+}
