69afa8c7792fac012e8c46e7a507175e9c2ed0b9
nanwan03 <wn1842@gmail.com>
2017-03-19 17:12:37 -0400

    commit

--- Linked List Cycle.java
+++ Linked List Cycle.java
@@ -16,20 +16,19 @@ public class Solution {
      */
     public boolean hasCycle(ListNode head) {  
         // write your code here
-        if (head == null) {
+        if (head == null || head.next == null) {
             return false;
         }
         ListNode slow = head;
-        ListNode fast = head;
-        do {
+        ListNode fast = head.next;
+        while (slow != fast) {
             if (fast != null && fast.next != null) {
                 slow = slow.next;
                 fast = fast.next.next;
             } else {
                 return false;
             }
-        } while (slow != fast);
+        }
         return true;
     }
 }
-
