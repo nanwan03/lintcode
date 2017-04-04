7b14d603e8aa732ac012a422b59806ea5e1560da
nanwan03 <wn1842@gmail.com>
2017-04-04 15:16:52 -0400

    commit

--- Implement Queue by Two Stacks.java
+++ Implement Queue by Two Stacks.java
@@ -1,8 +1,8 @@
-public class Solution {
+public class MyQueue {
     private Stack<Integer> stack1;
     private Stack<Integer> stack2;
 
-    public Solution() {
+    public MyQueue() {
        // do initialization if necessary
        stack1 = new Stack<Integer>();
        stack2 = new Stack<Integer>();
@@ -23,11 +23,10 @@ public class Solution {
     public int top() {
         // write your code here
         if (stack2.isEmpty()) {
-            while(!stack1.isEmpty()) {
+            while (!stack1.isEmpty()) {
                 stack2.push(stack1.pop());
             }
         }
         return stack2.peek();
     }
 }
-
