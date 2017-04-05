feb9c057e97aa0c5742e00c3cf6183002f9f35e4
nanwan03 <wn1842@gmail.com>
2017-04-05 15:57:58 -0400

    commit

--- Super Ugly Number.java
+++ Super Ugly Number.java
@@ -1,27 +1,3 @@
-class Number{
-    int un;
-    int pos;
-    int prime;
-    Number(int un, int pos, int prime){
-        this.un = un;
-        this.pos = pos;
-        this.prime = prime;
-    }
-}
-class NumberCompare implements Comparator<Number>{
-
-    @Override
-    public int compare(Number x, Number y) {
-        // TODO Auto-generated method stub
-        if (x.un > y.un)
-            return 1;
-        else if (x.un < y.un)
-            return -1;
-        else
-            return 0;
-    }
-}
-
 public class Solution {
     /**
      * @param n a positive integer
@@ -30,22 +6,18 @@ public class Solution {
      */
     public int nthSuperUglyNumber(int n, int[] primes) {
         // Write your code here
-        Comparator<Number> comparator = new NumberCompare();
-        PriorityQueue<Number> queue = 
-                new PriorityQueue<Number>(primes.length, comparator);
-        for(int i = 0; i < primes.length; i ++) 
-            queue.add(new Number(primes[i], 0, primes[i]));
-        int[] uglyNums = new int[n];
-        uglyNums[0] = 1;
-        for(int i = 1; i < n; i++){
-            Number min = queue.peek();
-            uglyNums[i] = min.un;
-            while(queue.peek().un == min.un){
-                Number tmp = queue.poll();
-                queue.add(new Number(uglyNums[tmp.pos + 1] * tmp.prime, tmp.pos+1, tmp.prime)); 
+        Queue<Long> heap = new PriorityQueue<Long>();
+        heap.offer(1L);
+        Set<Long> set = new HashSet<Long>();
+        set.add(1L);
+        while (--n > 0) {
+            long val = heap.poll();
+            for (int i : primes) {
+                if (set.add(val * i)) {
+                    heap.offer(val * i);
+                }
             }
         }
-    
-        return uglyNums[n-1];
+        return heap.peek().intValue();
     }
 }
