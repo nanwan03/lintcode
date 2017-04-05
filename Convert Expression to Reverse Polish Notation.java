3c22eb3507c4f58eb9101df3eb7dd8261ce8a77c
nanwan03 <wn1842@gmail.com>
2017-04-04 22:12:41 -0400

    commit

--- Convert Expression to Reverse Polish Notation.java
+++ Convert Expression to Reverse Polish Notation.java
@@ -6,57 +6,56 @@ public class Solution {
     public ArrayList<String> convertToRPN(String[] expression) {
         // write your code here
         if (expression == null || expression.length == 0) {
-    		return new ArrayList<String>();
-    	}
-    	String ops = "()+-*/";
-    	ArrayList<String> rpn = getRPN(expression, ops);
-    	return rpn;
+            return new ArrayList<String>();
+        }
+        return getRPN(expression);
     }
-    private ArrayList<String> getRPN(String[] expression, String ops) {
-    	ArrayList<String> rpn = new ArrayList<String>();
-    	Stack<String> stack = new Stack<String>();
-    	for (String str : expression) {
-    		if (str.equals("(") || str.equals(")") || str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/")) {
-    			if (stack.isEmpty()) {
-    				stack.push(str);
-    			} else if (str.equals("(")) {
-    				stack.push(str);
-    			} else if (str.equals(")")) {
-    				while (!stack.isEmpty()) {
-    					String op = stack.pop();
-    					if (op.equals("(")) {
-    						break;
-    					}
-    					rpn.add(op);
-    				}
-    			} else {
-    				if (mycmp(stack.peek(), str) == 1) {
-    					stack.push(str);
-    				} else {
-    					while (!stack.isEmpty() && mycmp(stack.peek(), str) == 0) {
-    						rpn.add(stack.pop());
-    					}
-    					stack.push(str);
-    				}
-    			}
-    		} else {
-    			rpn.add(str);
-    		}
-    	}
-    	while (!stack.isEmpty()) {
-    		rpn.add(stack.pop());
-    	}
-    	return rpn;
+    private ArrayList<String> getRPN(String[] expression) {
+        ArrayList<String> rpn = new ArrayList<String>();
+        Stack<String> stack = new Stack<String>();
+        String ops = "+-*/()";
+        for (String str : expression) {
+            if (ops.contains(str)) {
+                if (stack.isEmpty() || str.equals("(")) {
+                    stack.push(str);
+                } else if (str.equals(")")) {
+                    while (!stack.isEmpty()) {
+                        String op = stack.pop();
+                        if (op.equals("(")) {
+                            break;
+                        }
+                        rpn.add(op);
+                    }
+                } else {
+                    if (cmp(stack.peek(), str) == 1) {
+                        stack.push(str);
+                    } else {
+                        while (!stack.isEmpty() && cmp(stack.peek(), str) == 0) {
+                            rpn.add(stack.pop());
+                        }
+                        stack.push(str);
+                    }
+                }
+            } else {
+                rpn.add(str);
+            }
+        }
+        while (!stack.isEmpty()) {
+            rpn.add(stack.pop());
+        }
+        return rpn;
     }
-    private int mycmp(String a, String b) {
-    	if(b.equals("(")) {
-    		return 1;//左括号直接入栈
-    	} else if((b.equals("*") || b.equals("/")) &&(a.equals("+") || a.equals("-") || a.equals("("))) {
-    		return 1;//*、/优先级高于+、-、(，入栈
-    	} else if((b.equals("+") || b.equals("-")) && (a.equals("("))) {
-    		return 1;//+、-优先级高于(，入栈
-    	} else {
-    		return 0;
-    	}
+    private int cmp(String a, String b) {
+        String temp1 = "+-(";
+        String temp2 = "*/";
+        if (temp1.contains(a) && temp2.contains(b)) {
+            return 1;
+        }
+        temp1 = "(";
+        temp2 = "+-";
+        if (temp1.contains(a) && temp2.contains(b)) {
+            return 1;
+        }
+        return 0;
     }
 }
