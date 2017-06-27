c055ac21e9f78fc337ae6d88f07bc8ae38fc85ec
nanwan03 <wn1842@gmail.com>
2017-06-26 19:12:13 -0700

    commit

--- Decode Ways.java
+++ Decode Ways.java
@@ -5,13 +5,11 @@ public class Solution {
      */
     public int numDecodings(String s) {
         // Write your code here
-        if (s == null || s.length() == 0) {
+        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
             return 0;
         }
-        if (s.length() == 1) {
-            return s.charAt(0) == '0' ? 0 : 1;
-        }
-        int[] dp = new int[]{1, 1, 0};
+        
+        int[] dp = new int[]{1, 1, 1};
         for (int i = 1; i < s.length(); ++i) {
             dp[2] = 0;
             if (s.charAt(i) != '0') {
