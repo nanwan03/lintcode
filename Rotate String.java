7fbc077f2774ef52e010cf49960ee5e933ee60c6
nanwan03 <wn1842@gmail.com>
2017-07-21 22:58:57 -0700

    commit

--- Rotate String.java
+++ Rotate String.java
@@ -1,30 +1,27 @@
 public class Solution {
-    /*
-     * param A: A string
-     * param offset: Rotate string with offset.
-     * return: Rotated string.
+    /**
+     * @param str: an array of char
+     * @param offset: an integer
+     * @return: nothing
      */
-    public char[] rotateString(char[] A, int offset) {
-        // wirte your code here
-        if (A == null || A.length == 0) {
-            return A;
+    public void rotateString(char[] str, int offset) {
+        // write your code here
+        if (str == null || str.length == 0) {
+            return;
         }
-        offset = offset % A.length;
-        int left = 0;
-        int right = A.length - 1;
-        reverse(A, left, right - offset);
-        reverse(A, right - offset + 1, right);
-        reverse(A, left, right);
-        return A;
+        offset = offset % str.length;
+        reverse(str, 0, str.length - 1);
+        reverse(str, 0, offset - 1);
+        reverse(str, offset, str.length - 1);
     }
-    private void reverse(char[] A, int left, int right) {
+    private void reverse(char[] str, int left, int right) {
         while (left < right) {
-            swap(A, left++, right--);
+            swap(str, left++, right--);
         }
     }
-    private void swap(char[] A, int i, int j) {
-        char c = A[i];
-        A[i] = A[j];
-        A[j] = c;
+    private void swap(char[] str, int i, int j) {
+        char c = str[i];
+        str[i] = str[j];
+        str[j] = c;
     }
-};
+}
