b3f26d35d53c532ec914a449667a89b8183c1214
nanwan03 <wn1842@gmail.com>
2017-08-09 23:16:41 -0700

    commit

--- Decode Ways.java
+++ Decode Ways.java
@@ -8,20 +8,20 @@ public class Solution {
         if (s == null || s.length() == 0 || s.charAt(0) == '0') {
             return 0;
         }
-        
-        int[] dp = new int[]{1, 1, 1};
-        for (int i = 1; i < s.length(); ++i) {
-            dp[2] = 0;
-            if (s.charAt(i) != '0') {
-                dp[2] = dp[1];
-            }
-            int val = Integer.parseInt(s.substring(i - 1, i + 1));
-            if (10 <= val && val <= 26) {
-                dp[2] += dp[0];
-            }
+        int size = s.length();
+        int[] dp = new int[]{1, ways(s.charAt(0))};
+        for (int i = 1; i < size; ++i) {
+            int temp = (dp[1] * ways(s.charAt(i)) + dp[0] * ways(s.substring(i - 1, i + 1)));
             dp[0] = dp[1];
-            dp[1] = dp[2];
+            dp[1] = temp;
         }
-        return dp[2];
+        return dp[1];
+    }
+    private int ways(char c) {
+        return c == '0' ? 0 : 1;
+    }
+    private int ways(String sub) {
+        int val = Integer.parseInt(sub);
+        return 10 <= val && val <= 26 ? 1 : 0;
     }
 }
