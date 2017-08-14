739ff3de6ca108be22013a3913d18c5d160e08a2
nanwan03 <wn1842@gmail.com>
2017-08-13 17:28:54 -0700

    commit

--- LRU Cache - Solution I.java
+++ LRU Cache - Solution I.java
@@ -1,5 +1,4 @@
-public class Solution {
-
+public class LRUCache {
     private class Node {
         int key;
         int value;
@@ -8,18 +7,18 @@ public class Solution {
         Node(int key, int value) {
             this.key = key;
             this.value = value;
-            this.prev = null;
-            this.next = null;
+            this.prev = prev;
+            this.next = next;
         }
     }
-    private int size;
+    private int capacity;
     private Node head = new Node(-1, -1);
     private Node tail = new Node(-1, -1);
     private Map<Integer, Node> map = new HashMap<Integer, Node>();
     // @param capacity, an integer
-    public Solution(int capacity) {
+    public LRUCache(int capacity) {
         // write your code here
-        this.size = capacity;
+        this.capacity = capacity;
         head.next = tail;
         tail.prev = head;
     }
@@ -30,11 +29,10 @@ public class Solution {
         if (!map.containsKey(key)) {
             return -1;
         }
-        Node cur = map.get(key);
-        cur.next.prev = cur.prev;
-        cur.prev.next = cur.next;
-        moveToTail(cur);
-        return cur.value;
+        Node n = map.get(key);
+        removeNode(n);
+        moveToTail(n);
+        return n.value;
     }
 
     // @param key, an integer
@@ -42,23 +40,28 @@ public class Solution {
     // @return nothing
     public void set(int key, int value) {
         // write your code here
-        if (get(key) != -1) {
+        if (get(key) != -1)  {
             map.get(key).value = value;
             return;
         }
-        if (map.size() == size) {
+        if (map.size() == capacity) {
             map.remove(head.next.key);
-            head.next = head.next.next;
-            head.next.prev = head;
+            removeNode(head.next);
         }
         Node insert = new Node(key, value);
         map.put(key, insert);
         moveToTail(insert);
     }
-    private void moveToTail(Node cur) {
-        cur.next = tail;
-        cur.prev = tail.prev;
-        cur.next.prev = cur;
-        cur.prev.next = cur;
+    private void moveToTail(Node n) {
+        n.next = tail;
+        n.prev = tail.prev;
+        tail.prev.next = n;
+        tail.prev = n;
+    }
+    private void removeNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        node.next = null;
+        node.prev = null;
     }
 }
