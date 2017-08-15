ab9b9d55f8e71cc4efd1742d5bcecfba3a490951
nanwan03 <wn1842@gmail.com>
2017-08-14 17:42:43 -0700

    commit

--- Maximum Average Subarray.java
+++ Maximum Average Subarray.java
@@ -16,29 +16,30 @@ public class Solution {
         return helper(nums, k, (double)min, (double)max);
     }
     private double helper(int[] nums, int k, double left, double right) {
-        int size = nums.length;
-        double[] sum = new double[size + 1];
-        sum[0] = 0;
         while (right - left >= 1e-6) {
             double mid = (left + right) / 2.0;
-            double prevMin = 0;
-            boolean check = false;
-            for (int i = 1; i <= size; ++i) {
-                sum[i] = sum[i - 1] + nums[i - 1] - mid;
-                if (i >= k) {
-                    prevMin = Math.min(prevMin, sum[i - k]);
-                    if (sum[i] - prevMin >= 0) {
-                        check = true;
-                        break;
-                    }
-                }
-            }
-            if (check) {
+            if (isValid(nums, k, mid)) {
                 left = mid;
             } else {
                 right = mid;
             }
         }
         return left;
+    }
+    private boolean isValid(int[] nums, int k, double target) {
+        int size = nums.length;
+        double[] sum = new double[size + 1];
+        sum[0] = 0;
+        double prevMin = 0;
+        for (int i = 1; i <= size; ++i) {
+            sum[i] = sum[i - 1] + nums[i - 1] - target;
+            if (i >= k) {
+                prevMin = Math.min(prevMin, sum[i - k]);
+                if (sum[i] - prevMin >= 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
