05cbc508861208114dd762dc769d0dd7f1a18499
nanwan03 <wn1842@gmail.com>
2017-09-24 16:41:04 -0700

    commit

--- Consistent Hashing.java
+++ Consistent Hashing.java
@@ -5,30 +5,23 @@ public class Solution {
      */
     public List<List<Integer>> consistentHashing(int n) {
         // write your code here
-        List<List<Integer>> results = new ArrayList<List<Integer>>();
-        List<Integer> machine = new ArrayList<Integer>();
-        machine.add(0);
-        machine.add(359);
-        machine.add(1);
-        results.add(machine);
-        for (int i = 1; i < n; ++i) {
-            List<Integer> new_machine = new ArrayList<Integer>();
-            int index = 0;
-            for (int j = 1; j < i; ++j) {
-                if (results.get(j).get(1) - results.get(j).get(0) + 1 >
-                    results.get(index).get(1) - results.get(index).get(0) + 1)
-                    index = j;
-            }
-
-            int x = results.get(index).get(0);
-            int y = results.get(index).get(1);
-            results.get(index).set(1, (x + y) / 2);
-            
-            new_machine.add((x + y) / 2 + 1);
-            new_machine.add(y);
-            new_machine.add(i + 1);
-            results.add(new_machine);
+        List<List<Integer>> rst = new ArrayList<List<Integer>>();
+        if (n == 0) {
+            return rst;
         }
-        return results;
+        rst.add(Arrays.asList(new Integer[]{0, 359, 1}));
+        for (int i = 1; i < n; ++i) {
+            int index = 0;
+            for (int j = 0; j < i; ++j) {
+                if (rst.get(j).get(1) - rst.get(j).get(0) > rst.get(index).get(1) - rst.get(index).get(0)) {
+                    index = j;
+                }
+            }
+            int x = rst.get(index).get(0);
+            int y = rst.get(index).get(1);
+            rst.get(index).set(1, (x + y) / 2);
+            rst.add(Arrays.asList(new Integer[]{(x + y) / 2 + 1, y, i + 1}));
+        }
+        return rst;
     }
 }
