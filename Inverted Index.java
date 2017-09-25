7e9df422e387135f8f752745ff57a893181980dc
nanwan03 <wn1842@gmail.com>
2017-09-24 18:24:09 -0700

    commit

--- Inverted Index.java
+++ Inverted Index.java
@@ -12,31 +12,20 @@ public class Solution {
      */
     public Map<String, List<Integer>> invertedIndex(List<Document> docs) {
         // Write your code here
-        Map<String, List<Integer>> results = new HashMap<String, List<Integer>>();
+        Map<String, List<Integer>> map = new HashMap<String, List<Integer>>();
         for (Document doc : docs) {
             int id = doc.id;
-            StringBuffer temp = new StringBuffer("");
-            String content = doc.content;
-            int n = content.length();
-            for (int i = 0; i < n; ++i) {
-                if (content.charAt(i) == ' ') {
-                    insert(results, temp.toString(), id);
-                    temp = new StringBuffer("");
-                } else
-                    temp.append(content.charAt(i));
+            String[] strs = doc.content.split("\\s+");
+            for (String str : strs) {
+                if (!map.containsKey(str)) {
+                    map.put(str, new ArrayList<Integer>());
+                }
+                int size = map.get(str).size();
+                if (size == 0 || map.get(str).get(size - 1) != id) {
+                    map.get(str).add(id);
+                }
             }
-            insert(results, temp.toString(), id);
         }
-        return results;
-    }
-    private void insert(Map<String, List<Integer>> rt, String tmp, int id) {
-        if (tmp.equals("") || tmp == null)
-            return;
-        if (!rt.containsKey(tmp))
-            rt.put(tmp, new ArrayList<Integer>());
-        
-        int n = rt.get(tmp).size();
-        if (n == 0 || rt.get(tmp).get(n - 1) != id)
-            rt.get(tmp).add(id);
+        return map;
     }
 }
